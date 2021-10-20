15a254b0b66d7244563d0ed6df487bbda7e6dfa0
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-20 15:38:38 +0200

    variables requested by the user

---
+++ js/password.js
@@ -0,0 +1,22 @@
+// // chiedere nome, cognome e colore all'utente, 
+// poi generare una password con tutto unito 
+// + il numero 21
+
+
+// variaibili
+const userName;
+const userLastName;
+const color;
+let userPassword;
+
+// Chiedere all'utente di inserire il suo nome
+const userName = prompt("Inserisci il tuo nome");
+
+// Chiedere all'utente di inserire il suo cognome
+const userLastName = prompt("Inserisci il tuo cognome");
+
+
+// Chiedere all'utente di inserire il suo colore preferito
+const color = prompt("Inserisci il tuo colore preferito");
+
+
